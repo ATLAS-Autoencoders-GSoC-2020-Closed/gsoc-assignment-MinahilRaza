0d313555389836ecf5d2977f5e8100c1f9d335f0
Minahil <minahilrz@gmail.com>
2020-03-13 21:13:53 +0500

    Added training and testing scripts

--- nn_utils.py
+++ nn_utils.py
@@ -255,7 +255,39 @@ class AE_3D_200(nn.Module):
 
     def describe(self):
         return 'in-200-100-50-3-50-100-200-out'
+        
+class AE_3D_minahil(nn.Module):
+    def __init__(self, n_features=4):
+        super(AE_3D_minahil, self).__init__()
+        self.en1 = nn.Linear(n_features, 64)
+        self.en2 = nn.Linear(64, 128)
+        self.en3 = nn.Linear(128, 256)
+        self.en4 = nn.Linear(256, 100)
+        self.en5 = nn.Linear(100, 50)
+        self.en6 = nn.Linear(50, 24)
+        self.en7 = nn.Linear(24, 3)
+        
+        self.de1 = nn.Linear(3, 24)
+        self.de2 = nn.Linear(24, 50)
+        self.de3 = nn.Linear(50, 100)
+        self.de4 = nn.Linear(100, 256)
+        self.de5 = nn.Linear(256, 128)
+        self.de6 = nn.Linear(128, 64)
+        self.de7 = nn.Linear(64, n_features)
+        self.tanh = nn.Tanh()
 
+    def encode(self, x):
+        return self.en7(self.tanh(self.en6(self.tanh(self.en5(self.tanh(self.en4(self.tanh(self.en3(self.tanh(self.en2(self.tanh(self.en1(x)))))))))))))
+
+    def decode(self, x):
+        return self.de7(self.tanh(self.de6(self.tanh(self.de5(self.tanh(self.de4(self.tanh(self.de3(self.tanh(self.de2(self.tanh(self.de1(self.tanh(x))))))))))))))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
+
+    def describe(self):
+        return 'in-64-128-256-100-50-24-3-24-50-100-256-128-64-out'
 
 class AE_3D_small(nn.Module):
     def __init__(self, n_features=4):
